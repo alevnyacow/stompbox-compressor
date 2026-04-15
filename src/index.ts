d0ce6f5d05f248ca33769e8a7854ed3d049e7a17
alevny <alevnyacow@gmail.com>
2026-04-15 22:26:04 +0400

    feat: new contracts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ export type ErrorCreatingPayload = {
 };
 
 export interface ErrorModel {
-    name: string;
-    message: string;
     code: string;
     timestamp: number;
     details: any | null;
@@ -16,12 +14,7 @@ export const isError = (error: unknown): error is ErrorModel => {
         return false;
     }
 
-    const requiredKeys = [
-        'name',
-        'message',
-        'code',
-        'timestamp',
-    ];
+    const requiredKeys = ['code', 'timestamp'];
 
     const objKeys = Object.keys(error)
     return requiredKeys.every((x) => objKeys.includes(x));
@@ -35,28 +28,8 @@ export const isErrorWithCode = <T extends Record<string, string>>(errors: T) => 
     return error.code === errors[codeKey];
 }
 
-export const newError = <T extends Record<string, string>>(codes: T) => (errorType: keyof T, payload: ErrorCreatingPayload = { error: new Error('unknown error') }): ErrorModel => {
-    const { error, details } = payload
-    
-    let err: Error =
-        error instanceof Error ? error : new Error('unknown error');
-
-    if (typeof error === 'string' || typeof error === 'number') {
-        err = new Error(error?.toString());
-    }
-
-    if (typeof error === 'object' && error !== null) {
-        if (error instanceof Error) {
-            err = error;
-        } else {
-            const jsonRepresentation = JSON.stringify(error);
-            err = new Error(jsonRepresentation);
-        }
-    }
-
+export const newError = <T extends Record<string, string>>(codes: T) => (errorType: keyof T, details?: any): ErrorModel => {  
     return {
-        message: err.message,
-        name: err.name,
         timestamp: Date.now(),
         code: codes[errorType],
         details: details || null
