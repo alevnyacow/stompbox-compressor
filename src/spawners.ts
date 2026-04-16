308ad795d04015060a6396fea76fe094ed716fac
alevny <alevnyacow@gmail.com>
2026-04-16 12:59:55 +0400

    fix: removed old errors

--- src/spawners.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorModel } from "./base";
-
-export const base = <T extends Record<string, string>>(codes: T) => (errorType: keyof T, details?: any): ErrorModel => {  
-    return {
-        timestamp: Date.now(),
-        code: codes[errorType],
-        details: details || null
-    };
-};
