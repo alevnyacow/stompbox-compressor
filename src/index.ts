cf6c68643db3de776ca1fd79ac1dff6fa8ae3cf1
alevny <alevnyacow@gmail.com>
2026-04-17 17:46:23 +0400

    feat: limiter errors in value object

--- src/index.ts
+++ src/index.ts
@@ -54,11 +54,25 @@ export const CompressorValueObject = <
     public readonly model: Readonly<z.infer<typeof schema>>
 
     constructor(model: z.infer<typeof schema>) {
-      const parsedModel = ValueObject.schema.parse(model)
-      if (typeof parsedModel === 'object' && parsedModel) {
-        this.model = Object.freeze(parsedModel)
+      const { success, data, error } = schema.safeParse(model)
+      
+      if (!success) {
+        const { fieldErrors, formErrors }  = z.flattenError<any, string>(error, x => {
+            const path = x.path.length > 1 ? ` at ${x.path.slice(1).join('.')}` : ''
+            return `${x.message}${path}`
+        })
+
+        const fields = Object.fromEntries(Object.entries(fieldErrors).map(([key, errors]) => [key, errors!.join(', ')]))
+        const form = formErrors.length ? { generalErrors: formErrors.join(', ') } : {}
+
+        // @ts-ignore
+        throw new CompressorError('InvalidCreationPayload', { ...fields, ...form })
+      }
+
+      if (typeof data === 'object' && data) {
+        this.model = Object.freeze(data)
       } else {
-        this.model = parsedModel
+        this.model = data
       }
     }
   }
