33697f2e204e8bc3f7edf8211c273880166c9ad4
alevny <alevnyacow@gmail.com>
2026-04-17 16:57:28 +0400

    feat: CompressorEntity, CompressorValueObject
    BREAKING CHANGE: new contracts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-export * from './entities'
+import z, { ZodNumber, ZodObject, ZodRawShape, ZodString, ZodType } from 'zod'
+
+export const CompressorEntity = <
+  Shape extends ZodRawShape,
+  Id extends ZodString | ZodNumber = ZodString
+>(
+  schema: ZodObject<Shape>,
+  id?: Id
+) => {
+  const extendedSchema = schema.extend({
+    id: id ?? z.string().nonempty() as Id,
+  })
+
+  abstract class Entity {
+    public static schema = extendedSchema
+
+    private readonly _data: z.infer<typeof extendedSchema>
+
+    constructor(model: z.infer<typeof extendedSchema>) {
+      const parsedModel = extendedSchema.parse(model)
+      this._data = parsedModel
+    }
+
+    public get model(): Readonly<z.infer<typeof extendedSchema>> {
+        return Object.freeze(this._data)
+    }
+  }
+
+  return Entity
+}
+
+export const CompressorValueObject = <
+  Shape extends ZodType,
+>(
+  schema: Shape
+) => {
+  abstract class ValueObject {
+    public static schema = schema
+
+    private readonly _data: z.infer<typeof schema>
+
+    constructor(model: z.infer<typeof schema>) {
+      const parsedModel = ValueObject.schema.parse(model)
+      this._data = parsedModel
+    }
+
+    public get model(): Readonly<z.infer<typeof schema>> {
+        if (typeof this._data === 'object' && this._data) {
+            return Object.freeze(this._data)
+        }
+        return this._data
+    }
+  }
+
+  return ValueObject
+}
