c57b7c2a50c6429baf3e0c979fedfe03e7026517
alevny <alevnyacow@gmail.com>
2026-04-17 14:37:51 +0400

    feat: added limiter
    BREAKING CHANGE: changed contracts

--- src/entities.ts
+++ src/entities.ts
@@ -1,5 +1,4 @@
-import z, { ZodType } from 'zod'
-import { Errors } from './errors'
+import z, { ZodObject, ZodType } from 'zod'
 
 enum EntityErrors {
     INVALID_CREATION_PAYLOAD = 'ENTITY___INVALID_CREATION_PAYLOAD'
@@ -12,13 +11,30 @@ export const Entity = <T extends ZodType>(schema: T) => {
         public readonly model: z.infer<T>
 
         constructor(model: z.infer<T>) {
-            const parsedModel = schema.safeParse(model)
-            if (!parsedModel.success) {
-                Errors(EntityErrors).throw('INVALID_CREATION_PAYLOAD', parsedModel.error)
-            }
-            this.model = parsedModel.data!
+            const parsedModel = schema.parse(model)
+            // if (!parsedModel.success) {
+            //     Errors(EntityErrors).throw('INVALID_CREATION_PAYLOAD', parsedModel.error)
+            // }
+            this.model = parsedModel
         }
     }
 
     return CompressorEntity
+}
+
+export const EntityWithStringId = <T extends ZodObject>(schema: T) => {
+    class CompressorEntity {
+        public static schema = schema.extend({ id: z.string().nonempty() })
+
+        public readonly model: z.infer<T> & { id: string }
+
+        constructor(model: z.infer<T> & { id: string }) {
+            const parsedModel = CompressorEntity.schema.parse(model)
+            // if (!parsedModel.success) {
+            //     Errors(EntityErrors).throw('INVALID_CREATION_PAYLOAD', parsedModel.error)
+            // }
+            // @ts-ignore
+            this.model = parsedModel
+        }
+    }
 }
