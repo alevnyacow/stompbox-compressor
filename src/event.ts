5b51b23c8867297459c694e8741a134d43591fd2
alevny <alevnyacow@gmail.com>
2026-05-19 14:47:44 +0400

    Update event.ts

--- src/event.ts
+++ src/event.ts
@@ -1,16 +1,20 @@
 import { v4 } from "uuid";
-import { type EventEmitter } from "events";
+
+type Emitter = {
+    on: (eventName: string | symbol, handler: (...args: any[]) => void) => void;
+    emit: (eventName: string | symbol, payload: any) => void;
+};
 
 export const Event = <Payload>(name = v4()) => {
     abstract class Event {
         constructor(readonly payload: Payload) {}
 
-        fireInEmitter = (emitter: EventEmitter) => {
+        fireInEmitter = (emitter: Emitter) => {
             emitter.emit(name, this.payload);
         };
 
         static handleInEmitter = (
-            emitter: EventEmitter,
+            emitter: Emitter,
             handler: (payload: Payload) => Promise<void> | void,
         ) => {
             emitter.on(name, handler);
